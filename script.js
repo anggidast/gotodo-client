cb917adfcd7f346788bbd4d422259ec956ec2fda
anggidast <adastariana@gmail.com>
2021-06-04 23:21:52 +0700

    handle CRUD function

--- script.js
+++ script.js
@@ -29,6 +29,49 @@ $(document).ready(function () {
     localStorage.removeItem('access_token');
     auth();
   });
+
+  $('#add-btn').click(function (e) {
+    e.preventDefault();
+    $('#add-container').show();
+  });
+
+  $('#cancel-add-btn').click(function (e) {
+    e.preventDefault();
+    $('#add-container').hide();
+  });
+
+  $('#add-form').submit(function (e) {
+    e.preventDefault();
+    addTodo();
+    $('#add-container').hide();
+  });
+
+  $('#todos').on('click', '.edit-btn', function (e) {
+    e.preventDefault();
+    const id = $(this).data().id;
+    showEditTodo(id);
+    $('#edit-container').show();
+    $(window).scrollTop(0);
+    $('#submit-edit-btn').attr('data-id', id);
+  });
+
+  $('#edit-form').submit(function (e) {
+    e.preventDefault();
+    const id = $('#submit-edit-btn').data().id;
+    editTodo(id);
+    $('#edit-container').hide();
+  });
+
+  $('#cancel-edit-btn').click(function (e) {
+    e.preventDefault();
+    $('#edit-container').hide();
+  });
+
+  $('#todos').on('click', '.delete-btn', function (e) {
+    e.preventDefault();
+    const id = $(this).data().id;
+    deleteTodo(id);
+  });
 });
 
 function auth() {
@@ -37,12 +80,16 @@ function auth() {
     $('#reg-container').hide();
     $('#todos-container').show();
     $('#logout').show();
+    $('#add-container').hide();
+    $('#edit-container').hide();
 
     getTodos();
   } else {
     $('#login-container').show();
     $('#reg-container').hide();
     $('#todos-container').hide();
+    $('#add-container').hide();
+    $('#edit-container').hide();
     $('#logout').hide();
   }
 }
@@ -56,19 +103,20 @@ function getTodos() {
     },
   })
     .done(({ data }) => {
+      $('#todos').empty();
       data.forEach((el) => {
         $('#todos').append(
-          `<div class="list-group-item list-group-item-action">
-          <input class="form-check-input me-1" type="checkbox" value="" aria-label="..." />
-          <div class="d-flex w-100 justify-content-between">
-            <h5 class="mb-1">${el.title}</h5>
-            <small class="text-muted">3 days ago</small>
-          </div>
-          <p class="mb-1">${el.description}</p>
-          <small>Due date: ${new Date(el.due_date).toISOString().split`T`[0]}</small><br />
-          <button type="button" class="btn btn-sm btn-primary">Edit</button>
-          <button type="button" class="btn btn-sm btn-danger">Delete</button>
-        </div>`
+          `<div class="list-group-item">
+            <input class="form-check-input me-1" type="checkbox" value="" />
+            <div class="d-flex w-100 justify-content-between">
+              <h5 class="mb-1">${el.title}</h5>
+              <small class="text-muted">3 days ago</small>
+            </div>
+            <p class="mb-1">${el.description}</p>
+            <small>Due date: ${new Date(el.due_date).toISOString().split`T`[0]}</small><br />
+            <button type="button" class="btn btn-sm btn-primary edit-btn" data-id="${el.id}">Edit</button>
+            <button type="button" class="btn btn-sm btn-danger delete-btn" data-id="${el.id}">Delete</button>
+          </div>`
         );
       });
     })
@@ -106,6 +154,82 @@ function register() {
   })
     .done((res) => {
       auth();
+    })
+    .fail((err) => {
+      console.log(err);
+    });
+}
+
+function addTodo() {
+  $.ajax({
+    type: 'POST',
+    url: baseURL + '/todos',
+    headers: {
+      access_token: localStorage.getItem('access_token'),
+    },
+    data: {
+      title: $('#title').val(),
+      description: $('#description').val(),
+      due_date: $('#due_date').val(),
+    },
+  })
+    .done((res) => {
+      getTodos();
+    })
+    .fail((err) => {
+      console.log(err);
+    });
+}
+
+function showEditTodo(id) {
+  $.ajax({
+    type: 'GET',
+    url: baseURL + '/todos/' + id,
+    headers: {
+      access_token: localStorage.getItem('access_token'),
+    },
+  })
+    .done(({ data }) => {
+      $('#title-edit').val(data.title);
+      $('#description-edit').val(data.description);
+      $('#due_date-edit').val(new Date(data.due_date).toISOString().split`T`[0]);
+    })
+    .fail((err) => {
+      console.log(err);
+    });
+}
+
+function editTodo(id) {
+  $.ajax({
+    type: 'PUT',
+    url: baseURL + '/todos/' + id,
+    headers: {
+      access_token: localStorage.getItem('access_token'),
+    },
+    data: {
+      title: $('#title-edit').val(),
+      description: $('#description-edit').val(),
+      due_date: $('#due_date-edit').val(),
+    },
+  })
+    .done((res) => {
+      getTodos();
+    })
+    .fail((err) => {
+      console.log(err);
+    });
+}
+
+function deleteTodo(id) {
+  $.ajax({
+    type: 'DELETE',
+    url: baseURL + '/todos/' + id,
+    headers: {
+      access_token: localStorage.getItem('access_token'),
+    },
+  })
+    .done((res) => {
+      getTodos();
     })
     .fail((err) => {
       console.log(err);
